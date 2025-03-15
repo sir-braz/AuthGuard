f4b127d3c1e5f1b4c7989db4020486c8bc38b698
sir-braz <dev.vinicius-batista@hotmail.com>
2025-03-15 16:40:49 -0300

    Implementando a classe Role e estabelecer relacionamento Many-to-Many com User

---
+++ backend/src/main/java/com/pao/facil/paofacil_backend/entity/Role.java
@@ -0,0 +1,20 @@
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles") 
+    private Set<User> users;
+}
