a43be5e998bf8d8c70a2acc25265e9091a47016b
sir-braz <dev.vinicius-batista@hotmail.com>
2025-03-17 10:06:36 -0300

    Implementação simplificada do método invalidateToken no AuthService

--- backend/src/main/java/com/pao/facil/paofacil_backend/controller/AuthController.java
+++ backend/src/main/java/com/pao/facil/paofacil_backend/controller/AuthController.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
