b0fe3e3617816d957b19fe61e77777f53d53e0c6
sir-braz <dev.vinicius-batista@hotmail.com>
2025-03-21 13:01:53 -0300

    Atualizando o target

--- backend/src/main/java/com/pao/facil/paofacil_backend/controller/AuthController.java
+++ backend/src/main/java/com/pao/facil/paofacil_backend/controller/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "Content-Type", methods = {RequestMethod.GET, RequestMethod.POST})
+@CrossOrigin(origins = "*", allowedHeaders = "Content-Type", methods = {RequestMethod.GET, RequestMethod.POST})
 public class AuthController {
 
     private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
