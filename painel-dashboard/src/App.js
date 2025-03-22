b327e217e6a17e0455ae06192db852d904d296ac
sir-braz <dev.vinicius-batista@hotmail.com>
2025-03-22 19:56:37 -0300

    Removendo a estrutura de pastas que so foi um teste

--- painel-dashboard/src/App.js
+++ painel-dashboard/src/App.js
@@ -1,11 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import NewNavbar from './components/Navbar/NewNavbar'; 
 import Sidebar from './components/Sidebar/Sidebar'; 
-import Dashboard from './components/Dashboard/Dahboard'; 
-import Vendas from './components/Sales/Sales'; 
-import Estoque from './components/Stock/Stock'; 
-import Indicadores from './components/Indicator/Indicators'; 
 import Login from './components/Login/Login'; 
 import Register from './components/Register/Register';  
 import Profile from './components/Profile/Profile.js'
@@ -35,17 +30,12 @@ const App = () => {
   return (
     <Router>
       <div className="App">
-        {isAuthenticated && <NewNavbar onLogout={handleLogout} />}
         {isAuthenticated && <Sidebar />} 
         <div className={`content ${isAuthenticated ? 'with-sidebar' : ''}`}>
           <Routes>
             <Route path="/" element={<Navigate to={isAuthenticated ? "/home" : "/login"} />} />
             <Route path="/login" element={isAuthenticated ? <Navigate to="/home" /> : <Login setIsAuthenticated={handleLogin} />} />
             <Route path="/register" element={isAuthenticated ? <Navigate to="/home" /> : <Register />}  />
-            <Route path="/home" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-            <Route path="/vendas" element={<ProtectedRoute><Vendas /></ProtectedRoute>} />
-            <Route path="/estoque" element={<ProtectedRoute><Estoque /></ProtectedRoute>} />
-            <Route path="/indicators" element={<ProtectedRoute><Indicadores /></ProtectedRoute>} />
             <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
 
           </Routes>
