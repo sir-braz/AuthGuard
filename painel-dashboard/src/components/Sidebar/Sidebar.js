a7a1da79804243b963fd7ebd28f989033f4007ee
sir-braz <dev.vinicius-batista@hotmail.com>
2025-03-22 00:18:53 -0300

    Ajustes no App.css para garantir que o conteúdo do dashboard não fique sob a sidebar e melhorar a responsividade: - Adicionado CSS para ajustar o conteúdo ao tamanho da sidebar - Incluída transição suave ao abrir e fechar a sidebar - Melhorada a responsividade para diferentes tamanhos de tela

--- painel-dashboard/src/components/Sidebar/Sidebar.js
+++ painel-dashboard/src/components/Sidebar/Sidebar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { FaHome, FaChartBar, FaBox, FaUsers, FaBars, FaBreadSlice } from 'react-icons/fa';
+import { FaHome, FaChartBar, FaBox, FaUsers, FaBars, FaBreadSlice, FaUser } from 'react-icons/fa';
 import './Sidebar.css';
 
 const Sidebar = () => {
@@ -16,6 +16,9 @@ const Sidebar = () => {
       <div className="sidebar-header">
         <FaBreadSlice className="sidebar-logo" style={{ color: '#d4a373' }} />
         <FaBars className="sidebar-toggle" onClick={handleToggle} />
+      </div>
+      <div className="sidebar-user">
+        <FaUser className="me-2" /> Sir Braz
       </div>
       <Nav className="flex-column">
         <Nav.Link as={Link} to="/home" className="sidebar-link">
@@ -32,6 +35,9 @@ const Sidebar = () => {
         </Nav.Link>
         <Nav.Link as={Link} to="/indicators" className="sidebar-link">
           <FaChartBar className="me-2 icon-white" /> Indicadores
+        </Nav.Link>
+        <Nav.Link as={Link} to="/profile" className="sidebar-link">
+          <FaUser className="me-2 icon-white" /> Perfil
         </Nav.Link>
       </Nav>
     </div>
