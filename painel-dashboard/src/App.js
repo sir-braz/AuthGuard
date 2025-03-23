7b7d5fc4561f4456e5f2d044b0b23658895d43f0
sir-braz <dev.vinicius-batista@hotmail.com>
2025-03-23 09:16:43 -0300

    Substituir HashRouter por BrowserRouter para melhorar o roteamento da aplicação

--- painel-dashboard/src/App.js
+++ painel-dashboard/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Sidebar from './components/Sidebar/Sidebar';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
